b044650f4257ecbd1fda9acbce238b913366d6e4
HellcrowAndrey <bakamakafoqq@gmail.com>
2020-08-03 12:58:21 +0300

    update order service

--- orders/src/main/java/com/github/orders/service/IOrderDetailService.java
+++ orders/src/main/java/com/github/orders/service/IOrderDetailService.java
@@ -6,11 +6,19 @@ import com.github.orders.entity.OrderStatus;
 import java.util.List;
 
 public interface IOrderDetailService {
+
     OrderDetail crete(OrderDetail o);
+
     OrderDetail readById(Long orderId);
+
     OrderDetail readUserId(Long userId);
+
     List<OrderDetail> readAllUserId(Long userId);
+
     List<OrderDetail> readByStatus(OrderStatus status);
+
     void update(Long id, OrderStatus status);
+
     void update(OrderDetail o);
+
 }
