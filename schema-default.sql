58ebca18ed3aff7df398d97979c31641ecb55632
HellcrowAndrey <bakamakafoqq@gmail.com>
2020-08-05 16:11:50 +0300

    update docker-composer (default, dav, test, prod) and add sql schema (default, dav, test, prod)

---
+++ schema-default.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS "products_default.db";
+CREATE DATABASE "products_default.db";
+
+DROP DATABASE IF EXISTS "orders_default.db";
+CREATE DATABASE "orders_default.db";
+
+DROP DATABASE IF EXISTS "users_default.db";
+CREATE DATABASE "users_default.db";
+
+DROP DATABASE IF EXISTS "statistics_default.db";
+CREATE DATABASE "statistics_default.db";
---
+++ schema-dev.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS "products_dev.db";
+CREATE DATABASE "products_dev.db";
+
+DROP DATABASE IF EXISTS "orders_dev.db";
+CREATE DATABASE "orders_dev.db";
+
+DROP DATABASE IF EXISTS "users_dev.db";
+CREATE DATABASE "users_dev.db";
+
+DROP DATABASE IF EXISTS "statistics_dev.db";
+CREATE DATABASE "statistics_dev.db";
