87728467a9d9c1e90262c77f34817cf93cb97a1c
HellcrowAndrey <bakamakafoqq@gmail.com>
2020-08-06 11:12:18 +0300

    update entrypoint change delay 5 to 1

--- orders/orders-entrypoint.sh
+++ orders/orders-entrypoint.sh
@@ -9,7 +9,7 @@ cmd="$@"
 
 >&2 echo "container -> config-server for available!"
 
-while ! (nc -z $host $port); do sleep 5; echo 'Waiting for config-server services to start-up...'; done;
+while ! (nc -z $host $port); do sleep 1; echo 'Waiting for config-server services to start-up...'; done;
 
 >&2 echo "config-server is up - executing command"
 
