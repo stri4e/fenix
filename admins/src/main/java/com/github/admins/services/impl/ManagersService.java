12a6fe565a9d428eee24011ad8b71471ce505623
HellcrowAndrey <bakamakafoqq@gmail.com>
2020-08-19 18:02:07 +0300

    add admins and managers registrations

---
+++ admins/src/main/java/com/github/admins/services/impl/ManagersService.java
@@ -0,0 +1,15 @@
+package com.github.admins.services.impl;
+
+import com.github.admins.dto.UserRegDto;
+import com.github.admins.payload.Product;
+import com.github.admins.services.IManagersService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ManagersService implements IManagersService {
+    @Override
+    public void create(UserRegDto payload) {
+    }
+}
