1549424a67f287c0f1a6b96ae9cd632f00eae452
HellcrowAndrey <bakamakafoqq@gmail.com>
2020-10-28 18:39:36 +0200

    update token provide in orders, bitcoin and ethereum

--- ethereum/src/main/java/com/github/ethereum/utils/JwtTokenProvider.java
+++ ethereum/src/main/java/com/github/ethereum/utils/JwtTokenProvider.java
@@ -1,34 +1,21 @@
 package com.github.ethereum.utils;
 
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.JwsHeader;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 @Slf4j
 @Component
 public class JwtTokenProvider {
 
-    @Value(value = "#{${keys.store}}")
-    private Map<String, String> keysStore;
-
     public Long getUserFromJwt(String token) {
-        var claims = Jwts.parser()
-                .setSigningKey(this.keysStore.get(getKeyId(token)))
-                .parseClaimsJws(token)
-                .getBody();
-        return Long.parseLong(claims.getSubject());
+        return Long.parseLong(subject(token));
     }
 
-    private String getKeyId(String token) {
+    private String subject(String token) {
         var signatureIndex = token.lastIndexOf('.');
         var nonSignedToken = token.substring(0, signatureIndex + 1);
-        Header<?> h = Jwts.parser().parseClaimsJwt(nonSignedToken).getHeader();
-        return  (String) h.get(JwsHeader.KEY_ID);
+        return  Jwts.parser().parseClaimsJwt(nonSignedToken).getBody().getSubject();
     }
 
 }
