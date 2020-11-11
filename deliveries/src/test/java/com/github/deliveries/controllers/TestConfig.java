9dc5bca552114f60958d4319d9039cb5724e338c
HellcrowAndrey <bakamakafoqq@gmail.com>
2020-11-11 21:05:30 +0200

    update test configurations

---
+++ deliveries/src/test/java/com/github/deliveries/controllers/TestConfig.java
@@ -0,0 +1,23 @@
+package com.github.deliveries.controllers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import java.util.Properties;
+
+@Configuration
+public class TestConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() throws Exception {
+        PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+        Properties properties = new Properties();
+
+        properties.setProperty("swagger.enabled", "false");
+
+        pspc.setProperties(properties);
+        return pspc;
+    }
+
+}
