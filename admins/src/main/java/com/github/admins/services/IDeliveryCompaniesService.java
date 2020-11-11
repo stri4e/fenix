e6c079ca9c303c07a9199e669fba6f890df2e154
HellcrowAndrey <bakamakafoqq@gmail.com>
2020-11-11 20:00:28 +0200

    update deliveries in admins

--- admins/src/main/java/com/github/admins/services/IDeliveryCompaniesService.java
+++ admins/src/main/java/com/github/admins/services/IDeliveryCompaniesService.java
@@ -10,31 +10,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @FeignClient(
-        name = "orders-service",
+        name = "deliveries",
         fallback = DeliveryCompaniesService.class,
         contextId = "deliveryCompanyId"
 )
 public interface IDeliveryCompaniesService {
 
     @PostMapping(
-            path = "/v1/delivery/company/edit",
+            path = "/v1/companies/edit",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     @ResponseStatus(code = HttpStatus.CREATED)
     Optional<CompanyDto> save(@RequestBody CompanyDto payload);
 
     @GetMapping(
-            path = "/v1/delivery/company/fetch/{id}"
+            path = "/v1/companies/fetch/{id}"
     )
     Optional<CompanyDto> findById(@PathVariable(name = "id") Long id);
 
     @PutMapping(
-            path = "/v1/delivery/company/edit"
+            path = "/v1/companies/edit"
     )
     void update(@RequestBody CompanyDto payload);
 
     @DeleteMapping(
-            path = "/v1/delivery/company/edit/{id}"
+            path = "/v1/companies/edit/{id}"
     )
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     void delete(@PathVariable(name = "id") Long id);
