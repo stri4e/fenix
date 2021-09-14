4838fbf4eb883edf685b3e1e58d8b48ce508a79c
hellcrow <bakamakafoqq@gmail.com>
2021-09-14 20:23:30 +0300

    added new module employees-gateway

---
+++ employees-gateway/src/test/java/com/github/gateway/EmployeesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
