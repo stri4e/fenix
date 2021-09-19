eef1a42bf2285c3323d07a31b4e9d3a1db9a8e93
hellcrow <bakamakafoqq@gmail.com>
2021-09-19 22:30:29 +0300

    added crud to account controller

--- employees/src/main/java/com/github/employees/repository/AccountRepo.java
+++ employees/src/main/java/com/github/employees/repository/AccountRepo.java
@@ -3,7 +3,11 @@ package com.github.employees.repository;
 import com.github.employees.entities.Account;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @Repository
 public interface AccountRepo extends ReactiveMongoRepository<Account, String> {
+    Mono<Account> findByEmployeeId(UUID employeeId);
 }
