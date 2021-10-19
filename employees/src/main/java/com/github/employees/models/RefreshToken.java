b70fc5974d0493629749b486a9e45c628f8d2f6d
hellcrow <bakamakafoqq@gmail.com>
2021-10-19 22:11:48 +0300

    added RefreshToken model

---
+++ employees/src/main/java/com/github/employees/models/RefreshToken.java
@@ -0,0 +1,15 @@
+package com.github.employees.models;
+
+import java.util.Date;
+
+public class RefreshToken {
+
+    private String token;
+
+    private Date expireIn;
+
+    private String expireTime;
+
+    private String cookieName;
+
+}
